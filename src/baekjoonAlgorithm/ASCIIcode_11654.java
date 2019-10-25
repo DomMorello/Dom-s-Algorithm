d936f7293bbb75958f60b072b497d440c15c6036
Dom_Morello <zuzudnf@gmail.com>
2019-10-25 17:36:14 +0900

    #11654 아스키코드

---
+++ src/baekjoonAlgorithm/ASCIIcode_11654.java
@@ -0,0 +1,17 @@
+package baekjoonAlgorithm;
+import java.io.*;
+/**
+ * @author Dom_Morello
+ * @Project Algorithm
+ * @Date 2019. 10. 25.
+ */
+public class ASCIIcode_11654 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char input = br.readLine().charAt(0);
+		System.out.println((int)input);
+	}
+
+}
