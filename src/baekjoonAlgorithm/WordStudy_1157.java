60461bef3a41840584def1ee98a64aca9e61d639
Dom_Morello <zuzudnf@gmail.com>
2019-10-30 21:59:46 +0900

    #1157 단어 찾기

--- src/baekjoonAlgorithm/WordStudy_1157.java
+++ src/baekjoonAlgorithm/WordStudy_1157.java
@@ -12,7 +12,6 @@ public class WordStudy_1157 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
 		char[] chArr = input.toCharArray();	//알파벳 배열과 비교하기 위해서 char배열생성
@@ -23,7 +22,6 @@ public class WordStudy_1157 {
 		for(int i=0; i < alphabet.length; i++) {
 			alphabet[i] = (char)(i+65);
 		}
-//		System.out.println(Arrays.toString(alphabet));
 		//input문자열의 알파벳이 몇번 반복돼서 등장하는지 검사
 		for(int i=0; i < chArr.length; i++) {
 			for(int j=0; j < alphabet.length; j++) {
@@ -34,30 +32,26 @@ public class WordStudy_1157 {
 				}
 			}
 		}
-//		System.out.println(Arrays.toString(count));
 		//count배열에서 최댓값을 일단 구해보자
-		int max = count[0];
+		int max = count[0];	
 		for(int i=0; i < count.length; i++) {
 			if(max < count[i]) {
 				max = count[i];
 			}
 		}
-//		System.out.println(max);
 		//max값과 일치하는 위치의 char를 뽑아라
-		//dd
 		int location = 0;
-		int repeat = 0;
+		int repeat = 0;	//그런데 최댓값이 반복이 되면 ?를 출력하기 위해서 선언
 		for(int i=0; i < alphabet.length; i++) {
 			if(max == count[i]) {
-				repeat++;
-				location = i;
+				repeat++;	//최댓값이 2개 이상이면 repeat을 증가시킨다.
+				location = i;	//최댓값의 위치를 얻는다.
 			}
 		}
-//		System.out.println(location);
 		if(repeat > 1) {
-			System.out.println('?');
+			System.out.println('?');	//최댓값이 2개 이상이면 ? 를 출력
 		}else {
-			System.out.println(alphabet[location]);
+			System.out.println(alphabet[location]);	//최댓값이 1개면 location위치에 있는 문자를 출력
 		}
 	}
 }
