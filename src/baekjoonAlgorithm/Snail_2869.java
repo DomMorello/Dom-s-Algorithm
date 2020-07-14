fba35655b30fae0166990bd98b496203a5f7a1fb
Donghyeon Lee <donglee@c1r4s3.42seoul.kr>
2020-07-14 14:53:48 +0900

    restart

---
+++ src/baekjoonAlgorithm/Snail_2869.java
@@ -0,0 +1,17 @@
+package baekjoonAlgorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class Snail_2869 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = bf.readLine().split(" ");
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++)
+            arr[i] = Integer.parseInt(input[i]);
+        
+    }
+}
